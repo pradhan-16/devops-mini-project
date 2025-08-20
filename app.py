dad07ef88739ca0826a8a0741826f9c58beabc6c
piyush <piyush@piyush-Lenovo-IdeaPad-S340-14IWLsharmapiyush.gmail.com>
2025-08-21 04:11:11 +0530

    added about page route

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Devops"
 
+@app.route('/about')
+def about():
+    return "This is About Page - Learning DevOps Flow"
+
+
 if __name__ =="__main__":
    app.run(host="0.0.0.0" , port =5000) 
